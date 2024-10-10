eceb7f354820dff42b10e55ade43b533565d8a35
lizmanchego <karinamanchego0416@gmail.com>
2024-10-10 13:59:17 -0500

    [MAQ] Tema 4 Full

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -5,7 +5,182 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 4
-      h1 Titulo de primer nivel
+      h1 ¿Quién es el estudiante?
+    .bloque-texto-g.bloque-texto-g--inverso.color-acento-contenido.p-3.p-sm-4.p-md-5.mb-5
+      .bloque-texto-g__img.bg1
+        img.img-bloque.d-none.d-lg-block(src='@/assets/curso/temas/tema4/img1.png', alt='')
+        
+      .bloque-texto-g__texto.p-4
+        p.mb-0 El estudiante ha sido siempre el compañero esencial del profesor en el proceso de enseñanza-aprendizaje, evolucionando junto a los cambios pedagógicos, ha pasado de ser visto como una “tabula rasa” en el tradicionalismo, una copia del maestro en el conductismo, un ser que necesita afecto en las pedagogías activas, hasta convertirse en un individuo que piensa y toma posición crítica. Actualmente, se reconoce al estudiante como un ser motivado para aprender, con un cerebro capaz de realizar múltiples tareas, diversas inteligencias y estilos de aprendizaje que le permiten alcanzar sus objetivos formativos.
+
+    .bgimg4.mb-3
+      h3.pt-3 Discípulo, estudiante, discente, aprendiz y alumno
+    p.mb-5 Al igual que ocurre con los educadores, los educandos han recibido diferentes denominaciones a lo largo de la historia, según el contexto histórico y la naturaleza de la institución educativa en la que participan. Así, se utilizan términos como discípulo, estudiante, discente, aprendiz y alumno.
+    .row.justify-content-center.mb-5
+      .col-sm-12.col-lg-7
+        TabsA.color-acento-botones
+          .tarjeta.color-adicional-2.p-4(titulo="Discípulo")
+            h4 Discípulo
+            p.mb-0 La palabra “discípulo” proviene del latín discipulus, que significa “aprendiz”. Se refiere a una persona que aprende una doctrina, ciencia o arte bajo la guía de un maestro, y sigue las enseñanzas de una escuela, incluso si su tiempo de vida es posterior a la de sus maestros. Se caracteriza por una motivación intrínseca para aprender. Los maestros tienen discípulos.
+
+          .tarjeta.color-adicional-2.p-4(titulo="Estudiante")
+            h4 Estudiante
+            p.mb-0 El término “estudiante” es el más común para referirse a un educando. Viene del latín studium y se refiere a la persona que asiste a un lugar específico para aprender. Con el surgimiento de las escuelas, aparecieron los estudiantes. Los profesores tienen estudiantes.
+
+          .tarjeta.color-adicional-2.p-4(titulo="Discente")
+            h4 Discente
+            p.mb-0 Similar a “discípulo”, el término “discente” también proviene del latín discere (aprender), pero surgió más tarde, con la aparición de los primeros docentes. Se refiere a una persona que recibe enseñanza formal en una institución educativa. Los docentes tienen discentes.
+
+          .tarjeta.color-adicional-2.p-4(titulo="Aprendiz")
+            h4 Aprendiz
+            p.mb-0 La palabra “aprendiz” deriva del castellano antiguo aprendriz y del latín tardío apprendere. Se refiere a quien aprende un arte u oficio con el objetivo de desempeñarse en una profesión o trabajo. Los instructores tienen aprendices.
+
+          .tarjeta.color-adicional-2.p-4(titulo="Alumno")
+            h4 Alumno
+            p.mb-0  “Alumno” es una palabra controvertida. Anteriormente, se creía que provenía del latín lumen (luz) y el prefijo “a” (sin), lo que sugería que significaba “persona sin luz”. Sin embargo, estudios recientes desmienten esta interpretación. Actualmente, se acepta que “alumno” proviene de alere (alimentar, fortalecer), lo que lo define como alguien que se alimenta constantemente de conocimiento.
+      .col-sm-12.col-lg-5.d-none.d-lg-block
+        img(src='@/assets/curso/temas/tema4/img2.png')
+    .bgimg4.mb-3
+        h3.pt-3 Motivación: extrínseca, intrínseca, internalizada
+    p.mb-5 La motivación es un factor crucial en el proceso pedagógico, ya que impulsa el interés y la participación en las actividades de aprendizaje, es por esto por lo que, los expertos coinciden en que la desmotivación es una de las principales causas del fracaso escolar. Existen tres tipos principales de motivación: extrínseca, intrínseca e internalizada. 
+    .row.justify-content-center.mb-5
+      .col-sm-12.col-lg-3.d-none.d-lg-block
+        img(src='@/assets/curso/temas/tema4/img3.png', alt='')
+      .col-sm-12.col-lg-9.align-content-center  
+       .bgimg2.px-4.py-3
+        .bg4
+            SlyderF.mb-0(columnas="col-lg-12 col-xl-12")
+              .p-5.my-0.h-card
+                .row.justify-content-center.mb-0
+                  .col-sm-12.col-lg-12
+                    p.mb-0 La motivación extrínseca se basa en factores externos, como recompensas o castigos, lo que generalmente resulta en un aprendizaje débil y poco significativo, ya que desaparece cuando los refuerzos externos se eliminan.
+              .p-5.my-0.h-card
+                .row.justify-content-center.mb-0
+                  .col-sm-12.col-lg-12
+                    p.mb-0 La motivación intrínseca es cuando el estudiante se siente impulsado por la satisfacción personal de aprender y el desafío que esto representa, lo que lleva a un aprendizaje más profundo y continuo, aunque es menos común en los sistemas educativos que priorizan la evaluación y la imposición.
+              .p-5.my-0.h-card
+                .row.justify-content-center.mb-0
+                  .col-sm-12.col-lg-12
+                    p.mb-0 La motivación internalizada, en cambio, surge cuando los estudiantes adoptan valores y actitudes previamente inculcados por padres o maestros, asumiéndolos como propios. Este tipo de motivación no se basa ni en el disfrute inmediato de las actividades ni en refuerzos externos, sino en la integración de valores que permiten al estudiante construir su identidad y avanzar en su aprendizaje. Es el tipo de motivación más recomendable en los procesos educativos, ya que fomenta un compromiso más duradero y personal con el conocimiento.
+
+    Separador
+    #t_4_1.titulo-segundo.color-acento-contenido
+      h2 4.1  Su cerebro: su aprendizaje
+    p.mb-5 A partir de mediados del siglo XX, surgieron teorías cognitivas, constructivistas y de procesamiento de la información en la pedagogía, que otorgaron un papel central al cerebro como el principal gestor del aprendizaje humano. Estas teorías, desarrolladas por investigadores como Bruner, Piaget, Ausubel, Gagné y Siemens, subrayan que el proceso de aprendizaje está estrechamente relacionado con las actividades cerebrales, destacando la importancia de entender cómo funciona el cerebro para optimizar la educación, por ello revisemos lo siguiente:
+    .tarjeta--container.row.mb-5
+      .col-sm-12.col-lg-6.tarjeta.color-primario.p-5.mb-cus
+        .row.justify-content-start.align-items-center.mb-4
+          .col-sm-12.col-lg-2.d-none.d-lg-block
+              img(src='@/assets/curso/temas/tema4/img4.svg')
+          .col-sm-12.col-lg-8
+            h3.bg2.mb-0 Neuronas
+        ul.lista-ul--color.color-secundario
+          li.d-flex
+            i.fas.fa-chalkboard-teacher
+            span El aprendizaje humano comienza con la activación de miles de millones de neuronas que se conectan entre sí en el cerebro. 
+          li.d-flex
+            i.fas.fa-chalkboard-teacher
+            span Cuantas más conexiones se establecen entre las neuronas, mayor es el aprendizaje. Las neuronas poseen dendritas que reciben estímulos externos y axones que los conducen al núcleo celular. 
+          li.d-flex
+            i.fas.fa-chalkboard-teacher
+            span Al conectarse, liberan neurotransmisores que generan reacciones en cadena, creando nuevas conexiones con cada estímulo. 
+          li.d-flex
+            i.fas.fa-chalkboard-teacher
+            span Este proceso es rápido y se asocia principalmente con el aprendizaje emocional, como el juego y actividades cambiantes, siendo una etapa temprana del aprendizaje.
+      .col-sm-12.col-lg-6.tarjeta.color-secundario.p-5
+        .row.justify-content-start.align-items-center.mb-4
+          .col-sm-12.col-lg-2.d-none.d-lg-block
+              img(src='@/assets/curso/temas/tema4/img5.svg')
+          .col-sm-12.col-lg-8
+            h3.bg2.mb-0 Hemisferios y lóbulos
+        ul.lista-ul--color
+          li.d-flex
+            i.fas.fa-chalkboard-teacher
+            span La neo corteza cerebral se divide en dos hemisferios: el derecho, encargado de habilidades intuitivas y creativas, como la música y el arte, y el izquierdo, responsable del razonamiento lógico, matemático y lingüístico. 
+          li.d-flex
+            i.fas.fa-chalkboard-teacher
+            span Cada hemisferio está compuesto por cuatro lóbulos: el frontal (movimiento, raciocinio, lenguaje), parietal (sensibilidad, percepción), occipital (tareas visuales) y temporal (audición y equilibrio). 
+          li.d-flex
+            i.fas.fa-chalkboard-teacher
+            span Dependiendo del hemisferio, los lóbulos se especializan en habilidades verbales o no verbales. 
+          li.d-flex
+            i.fas.fa-chalkboard-teacher
+            span Los lóbulos prefrontales, ubicados en la parte frontal, coordinan procesos cognitivos complejos como la atención, memoria y toma de decisiones, esenciales en el aprendizaje más avanzado.
+
+
+    .bgimg5.mb-5
+      h3.py-1 Lóbulos y sus funciones
+    .row.justify-content-center.mb-5
+      .col-sm-12.col-lg-6
+        ImagenInfografica.color-acento-botones.mb-5
+          template(v-slot:imagen)
+              img(src='@/assets/curso/temas/tema4/img6.svg', alt='La infografía presenta una representación del cerebro humano, destacando cuatro lóbulos principales y sus respectivas funciones: Lóbulo frontal: Encargado del movimiento, el razonamiento, la resolución de problemas, el lenguaje y las emociones. Lóbulo occipital: Responsable de tareas visuales generales. Lóbulo temporal: Asociado con tareas visuales complejas, como el reconocimiento de rostros, así como el procesamiento auditivo. Lóbulo parietal: Relacionado con la sensibilidad, el tacto, la percepción del entorno, la presión, la temperatura y el olfato. Cada lóbulo está identificado y vinculado a sus funciones principales en el procesamiento cerebral.')
+          .tarjeta.color-acento-botones.p-3(x="68%" y="20%" numero="+")
+            .h5.mb-2 Lóbulo frontal
+            p.mb-0 Movimiento, raciocinio, resolución de problemas, lenguaje, emociones.
+          .tarjeta.color-acento-botones.p-3(x="40%" y="20%" numero="+")
+            .h5.mb-2 Lóbulo Parental
+            p.mb-0 Sensibilidad,	tacto,	percepción,	presión, temperatura y olor.
+          .tarjeta.color-acento-botones.p-3(x="50%" y="45%" numero="+")
+            .h5.mb-2 Lóbulo temporal
+            p.mb-0 Tareas visuales complejas como el reconocimiento de caras, además de la audición, equilibrio y coordinación.
+          .tarjeta.color-acento-botones.p-3(x="20%" y="35%" numero="+")
+            .h5.mb-2 Lóbulo occipital
+            p.mb-0 Tareas visuales generales.
+
+
+    Separador
+    #t_4_2.titulo-segundo.color-acento-contenido
+      h2 4.2  Estilos de aprendizaje
+    p.mb-5  Como se ha estudiado previamente en relación con las teorías del cerebro, el aprendizaje y las inteligencias múltiples, cada ser humano aprende de manera diferente, esto nos lleva a reflexionar sobre los estilos de aprendizaje, ya que cada persona, en función de sus inteligencias y la disposición de su cerebro, utiliza métodos y estrategias particulares para aprender.
+    .row.justify-content-center.mb-5
+      .col-sm-12.col-lg-5.d-none.d-lg-block
+        img(src='@/assets/curso/temas/tema4/img7.png', alt='')
+      .col-sm-12.col-lg-7
+        p.mb-4 La relevancia de los estilos de aprendizaje en las ciencias pedagógicas y didácticas ha sido tal que numerosos expertos han desarrollado diversas teorías y modelos al respecto. Entre ellos destacan:
+        ul.lista-ul--color
+          li.d-flex
+            i.fas.fa-chalkboard-teacher
+            span Modelo propuesto por el profesor norteamericano Ned Hermann (1992).
+          li.d-flex
+            i.fas.fa-chalkboard-teacher
+            span Modelo de los profesores norteamericanos.
+          li.d-flex
+            i.fas.fa-chalkboard-teacher
+            span Richard Felder y Linda Silverman (1988).
+          li.d-flex
+            i.fas.fa-chalkboard-teacher
+            span Modelo propuesto por el profesor David Kolb (1984).
+          li.d-flex
+            i.fas.fa-chalkboard-teacher
+            span Modelo de los profesores y psicólogos Richard Bandler y John Grinder (1972).
+          li.d-flex
+            i.fas.fa-chalkboard-teacher
+            span Modelo de hemisferios cerebrales (previamente estudiado).
+          li.d-flex
+            i.fas.fa-chalkboard-teacher
+            span Modelo de las inteligencias múltiples del profesor y psicólogo Howard Gardner (1983).
+
+    .row.justify-content-center.mb-5
+      .col-sm-12.col-lg-8
+        .br1.mb-5.p-4
+          h5 Para tener en cuenta:
+          p.mb-0  Aunque hay muchos más modelos, se han nombrado los que más difusión y relevancia han tenido en las ciencias pedagógicas y didácticas del siglo XX. Por lo tanto, si quiere identificar otras alternativas recuerde hacer sus consultas.
+        .bgimg1.p-4
+          h5 Para reflexionar: 
+          p.mb-0 ¿Prepara usted sus clases considerando los diferentes estilos de aprendizaje de sus estudiantes? Reflexione sobre la importancia de investigar y conocer estos estilos antes de iniciar un nuevo curso. Esta práctica, sin duda, enriquecerá su labor docente. Sus estudiantes son individuos únicos, llenos de talento y diversidad. El hecho de que algunos no destaquen en áreas como las matemáticas no significa que carezcan de inteligencia; simplemente, su fortaleza puede estar en otro ámbito. Tómese el tiempo para conocerlos y descubrir sus caminos hacia el aprendizaje. ¡No lo olvide!
+      .col-sm-12.col-lg-4.d-none.d-lg-block
+        img(src='@/assets/curso/temas/tema4/img8.png')
+
+
+
+
+
+
+
+
+
+
 
 </template>
 
